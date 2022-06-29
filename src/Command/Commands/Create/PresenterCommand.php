8a69966e213a0be5cff4d76164b744472399d51f
Luke Watts <watts.luke@yahoo.com>
2022-06-29 10:15:22 +0100

    Adding 'extends Presenter\Presenter' to 'create:presenter' command generated class

--- src/Command/Commands/Create/PresenterCommand.php
+++ src/Command/Commands/Create/PresenterCommand.php
@@ -32,11 +32,12 @@ class PresenterCommand extends Command
 
         $ClassGenerator = new PhpClassGenerator($class_name);
         $ClassGenerator->setNamespace('App\Presenter')
-            ->addUseStatement('Noesis\Presenter\PresenterInterface')
+            ->addUseStatement('Noesis\Presenter')
             ->addUseStatement('Psr\Http\Message\ServerRequestInterface')
             ->addUseStatement('Psr\Http\Message\ResponseInterface')
             ->addUseStatement('Cherif\InertiaPsr15\Middleware\InertiaMiddleware')
-            ->addImplements('PresenterInterface')
+            ->setExtends('Presenter\Presenter')
+            ->addImplements('Presenter\PresenterInterface')
             ->addMethod(
                 '__invoke',
                 'ResponseInterface',
--- src/PhpGenerator/PhpClassGenerator.php
+++ src/PhpGenerator/PhpClassGenerator.php
@@ -19,6 +19,7 @@ class PhpClassGenerator
     private string $namespace;
     private array $methods = [];
     private array $use_statements = [];
+    private string $extends;
     private array $implements = [];
     private string $output;
 
@@ -51,6 +52,13 @@ class PhpClassGenerator
         return $this;
     }
 
+    public function setExtends(string $extends): self
+    {
+        $this->extends = $extends;
+
+        return $this;
+    }
+
     public function addImplements(string $implements): self
     {
         $this->implements[] = $implements;
@@ -58,30 +66,44 @@ class PhpClassGenerator
         return $this;
     }
 
-    private function build()
+    private function appendOpeningScriptTag(array &$output): void
     {
-        $output = [];
         $output[] = ($this->strict_types) ? '<?php declare(strict_types=1);' : '<?php';
+    }
+
+    private function appendNamespaces(array &$output): void
+    {
         if (isset($this->namespace)) {
             $output[] = "namespace {$this->namespace};";
         }
+    }
 
-        $output[] = "";
-        
+    private function appendUseStatements(array &$output): void
+    {
         if (!empty($this->use_statements)) {
             foreach ($this->use_statements as $use_statement) {
                 $output[] = "use $use_statement;";
             }
             $output[] = "";
         }
+    }
 
+    private function appendClassDefinitionLine(array &$output): void
+    {
         $type = $this->type;
         $class_name = $this->class_name;
+        $has_extends = (is_array($this->implements) && count($this->implements) > 0);
         $implements = implode(', ', $this->implements);
         $has_implements = strlen(trim($implements)) > 0;
-        $output[] = ($has_implements) ? "$type $class_name implements $implements" : "$type $class_name";
-        $output[] = "{";
+        $line = "$type $class_name";
+        $line .= ($has_extends) ? " extends $this->extends" : '';
+        $line .= ($has_implements) ? " implements $implements" : '';
 
+        $output[] = $line;
+    }
+
+    private function appendMethods(array &$output): void
+    {
         if (!empty($this->methods)) {
             foreach ($this->methods as $method) {
                 /**
@@ -105,6 +127,22 @@ class PhpClassGenerator
                 $output[] = "";
             }
         }
+    }
+
+    private function build()
+    {
+        $output = [];
+        $this->appendOpeningScriptTag($output);
+        $this->appendNamespaces($output);
+
+        $output[] = "";
+        
+        $this->appendUseStatements($output);
+        
+        $this->appendClassDefinitionLine($output);
+        $output[] = "{";
+
+        $this->appendMethods($output);
 
         $output[] = "}";
         $output[] = "";

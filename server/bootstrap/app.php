b6010c2f6baa7c02497652069893a925133ef3db
Luke Watts <watts.luke@yahoo.com>
2022-07-02 08:54:05 +0100

    Adding .env support

--- server/bootstrap/app.php
+++ server/bootstrap/app.php
@@ -2,11 +2,15 @@
 
 require_once dirname(dirname(__DIR__)) . '/vendor/autoload.php';
 
-use Noesis\App\Container\ContainerInvoker;
 use Noesis\App\Middleware\AppMiddlewareInvoker;
+use Noesis\App\Container\ContainerInvoker;
 use Noesis\App\Router\ApiRouterInvoker;
 use Noesis\App\Router\WebRouterInvoker;
 use Noesis\App\AppFactory;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(dirname(dirname(__DIR__)));
+$dotenv->load();
 
 $app = AppFactory::createFromContainer((new ContainerInvoker)());
 $app = (new AppMiddlewareInvoker)($app);

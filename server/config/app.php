4f01e7aa87e0814264481fe002aee0b658b03e91
Luke Watts <watts.luke@yahoo.com>
2022-07-13 21:52:10 +0100

    Adding OAuth provider config to .env.example

--- server/config/app.php
+++ server/config/app.php
@@ -3,13 +3,19 @@
 use Monolog\Handler\StreamHandler;
 use Monolog\Level;
 
+$root = dirname(dirname(__DIR__));
+
 return [
     'environment' => 'dev',
-    'views_directory' => dirname(dirname(__DIR__)) . '/server/resources/views',
+    'db' => [
+        'connection' => env('DB_CONNECTION', 'sqlite'),
+        'database'   => $root . env('DB_DATABASE', "/db/dev.db")
+    ],
+    'views_directory' => "$root/server/resources/views",
     'logs' => [
         'channel'   => 'logs',
         'handler'   => StreamHandler::class,
-        'path'      => dirname(dirname(__DIR__)) . '/server/logs/{date}_{channel}.log',
+        'path'      => "$root/server/logs/{date}_{channel}.log",
         'level'     => Level::Info
     ],
     'oauth' => [
@@ -17,7 +23,18 @@ return [
             'client_id'     => env('GITHUB_OAUTH_CLIENT_ID'),
             'client_secret' => env('GITHUB_OAUTH_SECRET'),
             'redirect_uri'  => env('GITHUB_OAUTH_REDIRECT_URI')
+        ],
+        'linkedin' => [
+            'client_id'     => env('LINKEDIN_OAUTH_CLIENT_ID'),
+            'client_secret' => env('LINKEDIN_OAUTH_SECRET'),
+            'redirect_uri'  => env('LINKEDIN_OAUTH_REDIRECT_URI')
+        ],
+        'twitter' => [
+            'consumer_key'    => env('TWITTER_OAUTH_CONSUMER_KEY'),
+            'consumer_secret' => env('TWITTER_OAUTH_CONSUMER_SECRET'),
+            'client_id'       => env('TWITTER_OAUTH_CLIENT_ID'),
+            'client_secret'   => env('TWITTER_OAUTH_CLIENT_SECRET'),
+            'redirect_uri'    => env('TWITTER_OAUTH_REDIRECT_URI')
         ]
-        
     ]
 ];

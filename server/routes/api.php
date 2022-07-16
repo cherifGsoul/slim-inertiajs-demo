18b680c717eafa54234b587a7c64ac860b0d1eb9
Luke Watts <watts.luke@yahoo.com>
2022-07-16 12:49:20 +0100

    Fixing code analysis errors

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,9 +1,10 @@
 <?php declare(strict_types=1);
 
 use Laminas\Diactoros\Response\JsonResponse;
+use Slim\Routing\RouteCollectorProxy;
 
 // Define API routes here. They will automatically be prefixed with /api
+/** @var RouteCollectorProxy $route */
 $route->get('/user', function() {
-    $response = new JsonResponse(['name' => 'Luke Watts']);
-    return $response;
+    return new JsonResponse(['name' => 'Luke Watts']);
 });
